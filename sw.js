adc67ed6c801624128f93595bb971d68ff718854
mariadcfelicio-16 <mariadcfelicio@gmail.com>
2026-08-23 23:03:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='banheiro-apto-101-v4-2-ui';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/capa-v4.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request))));
+const CACHE='banheiro-apto-101-v4-3-pwa';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/capa-v4.png','./assets/icon-192.png','./assets/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
